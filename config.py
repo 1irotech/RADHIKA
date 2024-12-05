06a1eb31fed84b01bb7c9cdcbf1dad51912d1565
1irotech <irotech015@gmail.com>
2024-12-05 15:29:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "16457832"
+API_ID = "22169867"
 # -------------------------------------------------------------
-API_HASH = "3030874d0befdb5d05597deacc3e83ab"
+API_HASH = "cb5d60ad1402ce922d53bc500abd20df"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAHONy8JYrwzoCGNCzVcnADsjeRHjVOjbwY")
+BOT_TOKEN = getenv("BOT_TOKEN", "7921939461:AAGTRUTXXnNBlVasG7mMoqmB3vUTHuOi6j0")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
-SUPPORT_GRP = "+OL6jdTL7JAJjYzVl"
-UPDATE_CHNL = "BABY09_WORLD"
-OWNER_USERNAME = "UTTAM470"
+OWNER_ID = int(getenv("OWNER_ID", "7086360370"))
+SUPPORT_GRP = "ironmanhindigming"
+UPDATE_CHNL = "ironmanhindigaming88"
+OWNER_USERNAME = "Ironmanhindigaming"
